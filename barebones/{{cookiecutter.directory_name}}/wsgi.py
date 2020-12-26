18487ef395e96f8d20f60b256ccf13fa087671d2
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2020-12-26 22:13:46 +0530

    feat: add check for config file

--- barebones/{{cookiecutter.directory_name}}/wsgi.py
+++ barebones/{{cookiecutter.directory_name}}/wsgi.py
@@ -1,6 +1,13 @@
+
+import pathlib
+
 from {{ cookiecutter.app_slug }} import create_app
 
 # if you'd like to use an instance folder, then copy the `config/instance_template.py`
 # file and edit all the placeholder values.
 
-app = create_app(config_file="config.py")
+config_file = pathlib.Path("instance/config.py")
+if not config_file.is_file():
+    raise EnvironmentError("The config file {} does not exist".format(config_file.absolute()))
+
+app = create_app(config_file=config_file.absolute())
